46251989099c440fd03f4f381ff28548fbad488e
Henil490 <71130622+Henil490@users.noreply.github.com>
2020-12-14 19:49:32 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -1,27 +1,26 @@
 class Paper {
-    constructor(x, y, width, height) {
-        var options = {
-            isStatic:false,
-            restitution:0.3,
-            friction:0.5,
-            density:1.2
-
-        } 
-      this.body = Bodies.circle( x,y,radius,options);
-      this.radius = radius;
-      
+   constructor(x, y,radius ) {
+      var options = {
+          'restitution':0.3,
+          'friction':0.5,
+          'density':1.2
+      }
+      this.radius=radius
+      this.body = Bodies.circle(x, y, radius, options);
       
       World.add(world, this.body);
     }
-    
     display(){
-     var pos=this.body.position;
+      var pos =this.body.position;
+      
       push();
-     
-      
-      rectMode(CENTER);
-      
-      rect(pos.x,pos.y , this.width, this.height);
+      //translate(pos.x, pos.y);
+      //rotate(angle);
+      ellipseMode(RADIUS);
+      strokeWeight(8);
+      stroke("yellow");
+      fill("purple");
+      ellipse(pos.x, pos.y, this.radius, this.radius);
       pop();
     }
   };
